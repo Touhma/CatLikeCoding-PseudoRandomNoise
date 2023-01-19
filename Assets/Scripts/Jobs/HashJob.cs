bf9920f22be2383bf7a54766bec070ec05d7ef27
Touhma <Touhma@outlook.com>
2023-01-18 19:40:27 -0500

    https://catlikecoding.com/unity/tutorials/pseudorandom-noise/hashing/

---
+++ Assets/Scripts/Jobs/HashJob.cs
@@ -0,0 +1,30 @@
+﻿using _Utils;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+
+using static Unity.Mathematics.math;
+
+namespace Jobs
+{
+    [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
+    public struct HashJob: IJobFor {
+
+        [WriteOnly]
+        public NativeArray<uint> hashes;
+        
+        public int resolution;
+
+        public float invResolution;
+        
+        public SmallXXHash hash;
+        
+        public void Execute(int i) {
+            int v = (int)floor(invResolution * i + 0.00001f);
+            int u = i - resolution * v - resolution / 2;
+            v -= resolution / 2;
+
+            hashes[i] = hash.Eat(u).Eat(v);
+        }
+    }
+}
---
+++ Assets/Scripts/MonoBehaviours/HashVisualization.cs
@@ -0,0 +1,78 @@
+﻿using _Utils;
+using Jobs;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+namespace MonoBehaviours
+{
+    public class HashVisualization : MonoBehaviour
+    {
+        private static int
+            hashesId = Shader.PropertyToID("_Hashes"),
+            configId = Shader.PropertyToID("_Config");
+
+        [SerializeField] private Mesh instanceMesh;
+
+        [SerializeField] private Material material;
+
+        [SerializeField, Range(1, 512)] private int resolution = 16;
+        
+        [SerializeField] private int seed;
+        
+        [SerializeField, Range(-2f, 2f)] private float verticalOffset = 1f;
+
+        private NativeArray<uint> hashes;
+
+        private ComputeBuffer hashesBuffer;
+
+        private MaterialPropertyBlock propertyBlock;
+
+        private void OnEnable()
+        {
+            Debug.Log("OnEnable");
+            int length = resolution * resolution;
+            hashes = new NativeArray<uint>(length, Allocator.Persistent);
+            hashesBuffer = new ComputeBuffer(length, 4);
+
+            new HashJob
+            {
+                hashes = hashes,
+                resolution = resolution,
+                invResolution = 1f / resolution,
+                hash = SmallXXHash.Seed(seed)
+            }.ScheduleParallel(hashes.Length, resolution, default).Complete();
+
+            hashesBuffer.SetData(hashes);
+
+            propertyBlock ??= new MaterialPropertyBlock();
+            propertyBlock.SetBuffer(hashesId, hashesBuffer);
+            propertyBlock.SetVector(configId, new Vector4(resolution, 1f / resolution, verticalOffset / resolution));
+        }
+
+        private void OnDisable()
+        {
+            Debug.Log("OnDisable");
+            hashes.Dispose();
+            hashesBuffer.Release();
+            hashesBuffer = null;
+        }
+        
+        void Update () {
+            Graphics.DrawMeshInstancedProcedural(
+                instanceMesh, 0, material, new Bounds(Vector3.zero, Vector3.one),
+                hashes.Length, propertyBlock
+            );
+        }
+
+        private void OnValidate()
+        {
+            Debug.Log("OnValidate");
+            if (hashesBuffer != null && enabled)
+            {
+                OnDisable();
+                OnEnable();
+            }
+        }
+    }
+}
